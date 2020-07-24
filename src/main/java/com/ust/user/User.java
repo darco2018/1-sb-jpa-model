cfd4d489410b765909af8c9fb9ee2e593080ef66
darco2018 <newsecond@outlook.com>
2020-07-24 07:27:12 +0200

    redo User class

--- src/main/java/com/ust/user/User.java
+++ src/main/java/com/ust/user/User.java
@@ -7,19 +7,26 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Data
-@AllArgsConstructor @NoArgsConstructor
-@Table(name="users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
 @Entity
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    private String email;
+    private String password;
+    private String role;
 
-    public User(String username){
+    public User(String username, String email, String password, String role) {
         this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
     }
-
 }
 
 
